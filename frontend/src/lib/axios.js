69b949372dcc44a52e2a0281891bc11dbff91ab6
Himanshu <himanshusharmaktm@gmail.com>
2026-07-13 22:55:17 +0545

    Some changes for deployment preparation

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"        // So that you don't have to type it everytime
+    baseURL: BASE_URL,        // So that you don't have to type it everytime
 });
 
 export default api;
